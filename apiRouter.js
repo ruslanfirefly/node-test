521c98c5f9602a46f894c09e60d75229e0a81e25
Ruslan Serkin <ruslan.ru@gmail.com>
2017-11-02 22:30:45 +0300

    add mongo

--- apiRouter.js
+++ apiRouter.js
@@ -1,17 +1,37 @@
 const express = require("express");
 
-const router = express.Router()
-
-router.get("/", (req, res) => {
-  res.json({
-    hello: "world"
+module.exports = (db)=>{
+  const router = express.Router()
+  
+  router.get("/posts", (req, res) => {
+    db.Post.find()
+    .then(data=>{
+      res.json(data)
+    })
   })
-})
-
-router.get("/test/:name", (req, res) => {
-  res.json({
-    hello: req.params.name
+  router.post("/posts", (req, res)=>{
+    const post  = new db.Post(req.body)
+    post.save(function(err){
+      res.json(err);
+    }) // _id
   })
-})
-
-module.exports = router
+  router.get("/test_data", (req, res) => {
+   for(let i=0; i<101; i++){
+     (new db.TestData({
+       title: `test title ${i}`,
+       autor: i, 
+       text: `text_${i}`,
+       custom_field: (i+1) 
+     })).save()
+   }
+   res.json({finish:"done"})
+  })
+  
+  router.get("/test/:name", (req, res) => {
+    res.json({
+      hello: req.params.name
+    })
+  })
+  
+   return router
+}
--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const router = require("./apiRouter")
+const db  = require("./db")
+const bodyParser = require('body-parser')
 
+
+app.use(bodyParser.json())
 app.set('view engine', 'pug')
 app.set('views', './views')
 
 app.use("/static", express.static("public"))
 
-app.use("/api/v2", router)
+app.use("/api/v2", router(db))
 
 app.get('/', function (req, res) {
 
